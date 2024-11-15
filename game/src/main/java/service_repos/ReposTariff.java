6ac4f71a8167177ecb5756be242d406745d14744
tank <tankoman228@gmail.com>
2024-11-15 15:10:26 +0700

    port_launch_fix

--- game/src/main/java/service_repos/ReposTariff.java
+++ game/src/main/java/service_repos/ReposTariff.java
@@ -6,11 +6,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import com.cubes_and_mods.game.db.Tariff;
 
 import jakarta.transaction.Transactional;
 
+@Repository
 public interface ReposTariff extends JpaRepository<Tariff, Integer> {
 
 }
