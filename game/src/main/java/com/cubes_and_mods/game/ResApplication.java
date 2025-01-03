43fcc98003a2cae24459d1e6977f6a81adf478af
Tank 228 <92095847+tankoman228@users.noreply.github.com>
2025-01-03 22:31:02 +0700

    Починил сокет консоли

--- game/src/main/java/com/cubes_and_mods/game/ResApplication.java
+++ game/src/main/java/com/cubes_and_mods/game/ResApplication.java
@@ -2,11 +2,20 @@ package com.cubes_and_mods.game;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 import com.cubes_and_mods.game.service.Config;
 
 @SpringBootApplication
-//@EnableJpaRepositories(basePackages = {"service_repos.repos"})
+@ComponentScan(basePackages = {
+		"com.cubes_and_mods.game", 
+		"com.cubes_and_mods.game.repos", 
+		"com.cubes_and_mods.game.service", 
+		"com.cubes_and_mods.game.service.mineserver_process",
+		"com.cubes_and_mods.game.controller", 
+		"com.cubes_and_mods.game.db"
+})
 public class ResApplication {
 
 	public static void main(String[] args) {
