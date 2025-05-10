d227f14f4b126431176036249fa50b2c596a7fe7
Tank 228 <92095847+tankoman228@users.noreply.github.com>
2025-05-10 13:14:21 +0700

    Набросок API для servers

--- servers/src/main/java/com/cubes_and_mods/servers/controller/ControllerHosts.java
+++ servers/src/main/java/com/cubes_and_mods/servers/controller/ControllerHosts.java
@@ -1,5 +1,6 @@
 package com.cubes_and_mods.servers.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,8 +11,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.cubes_and_mods.servers.jpa.Client;
+import com.cubes_and_mods.servers.jpa.Host;
+import com.cubes_and_mods.servers.jpa.HostSharing;
+import com.cubes_and_mods.servers.jpa.repos.ClientRepos;
+import com.cubes_and_mods.servers.jpa.repos.HostRepos;
+import com.cubes_and_mods.servers.jpa.repos.HostSharingRepos;
+import com.cubes_and_mods.servers.security.ProtectedRequest;
+import com.cubes_and_mods.servers.security.annotations.AllowedOrigins;
+import com.cubes_and_mods.servers.security.annotations.AllowedOrigins.MService;
 
 /**
  * Getter of mineservers for one user, for all users, getting mineserver obj from DB by ID
@@ -20,45 +32,84 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/hosts")
 public class ControllerHosts {
 	
-	@GetMapping("/of_server/{id}")
-	public ResponseEntity<Void> of_server(){ return ResponseEntity.status(HttpStatus.NOT_IMPLEMENTED).build(); }
-	
-	@GetMapping("/of_user/{id}")
-	public ResponseEntity<Void> of_user(){ return ResponseEntity.status(HttpStatus.NOT_IMPLEMENTED).build(); }
+	@Autowired
+	private HostRepos hostRepos;
+
+	@Autowired
+	private ClientRepos clientRepos;
+
+	@Autowired
+	private HostSharingRepos hostSharingRepos;
+
+
+	@PostMapping("/of_user/{id}")
+	@AllowedOrigins(MService.WEB)
+	public ResponseEntity<List<Host>> of_user(@RequestBody ProtectedRequest<Void> request, @PathVariable Integer id) { 
+
+		Client user = new Client(); // TODO: get user from request
+		
+		/* 
+		var hosts = user.getHosts();
+		var sharings = user.getHost_sharings();
+
+		List<Host> res = new ArrayList<>();
+		res.addAll(hosts);
+		for (var sh : sharings) {
+			if (res.stream().anyMatch(h -> h.getId().equals((sh.getHost().getId())))) continue;
+			res.add(sh.getHost());
+		}*/
+
+		return ResponseEntity.ok(hostRepos.findAll());
+	}
 	
 	@GetMapping("/{id}")
-	public ResponseEntity<Void> id(){ return ResponseEntity.status(HttpStatus.NOT_IMPLEMENTED).build(); }
+	@AllowedOrigins(MService.WEB)
+	public ResponseEntity<Host> id(@RequestBody ProtectedRequest<Void> request, @PathVariable Integer id){ 
+		return ResponseEntity.ok(hostRepos.findById(id).get());
+	}
 	
 	@PutMapping("/{id}/edit")
-	public ResponseEntity<Void> edit(){ return ResponseEntity.status(HttpStatus.NOT_IMPLEMENTED).build(); }
+	@AllowedOrigins(MService.WEB)
+	public ResponseEntity<Void> edit(@RequestBody ProtectedRequest<Host> request, @PathVariable Integer id){ 
+		
+		// TODO: проверить, что так делать вообще можно этому юзеру
+		var host = hostRepos.findById(id);
+		if (host.isEmpty()) return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+
+		request.data.setId(id);
+		request.data.setMemoryUsed(host.get().getMemoryUsed());
+		request.data.setSecondsWorking(host.get().getSecondsWorking());
+		request.data.setTariff(host.get().getTariff());
+
+		hostRepos.save(request.data);
+		hostRepos.flush();
+
+		return ResponseEntity.status(HttpStatus.OK).build(); 
+	}
 	
 	@PostMapping("/{id}/share")
-	public ResponseEntity<Void> share(){ return ResponseEntity.status(HttpStatus.NOT_IMPLEMENTED).build(); }
-	
-	
-	/*
-	@Autowired
-	private ServiceMineservers serviceMineservers;
-	
-    @PostMapping("/all/{id}")
-    public ResponseEntity<List<Mineserver>> getAllOfUser(@PathVariable Integer id) {
-		return new ResponseEntity<>(serviceMineservers.findByIdOfUser(id), HttpStatus.OK);
-    }
-    
-    @PostMapping("/all")
-    public ResponseEntity<List<Mineserver>> getAll() {
-		return new ResponseEntity<>(serviceMineservers.findAll(), HttpStatus.OK);
-    }
-    
-    @PostMapping("/{id}")
-    public ResponseEntity<Mineserver> get(@PathVariable Integer id) {
-		return new ResponseEntity<>(serviceMineservers.findById(id), HttpStatus.OK);
-    }
-    
-    // DO NOT INVOKE THIS METHOD! USED IN ADMIN PANEL ONLY. After deleting youl'll have a lot of garbage files
-    @DeleteMapping("/delete/{id}")
-    public ResponseEntity<Void> delete(@PathVariable Integer id) {
-    	serviceMineservers.delete(id);
-		return new ResponseEntity<>(HttpStatus.OK);
-    }*/
+	@AllowedOrigins(MService.WEB)
+	public ResponseEntity<Void> share(@RequestBody ProtectedRequest<String> request, @PathVariable Integer id){ 
+		
+		// TODO: проверки на null и права
+		Client shared = clientRepos.findAll().stream().filter(x -> x.getEmail() == request.data).findFirst().get();
+		Client owner = hostRepos.findById(id).get().getClient();
+
+		var sharing = hostSharingRepos.findAll().stream().filter(x -> x.getClient().getId().equals(shared.getId()) && x.getHost().getId().equals(id)).findFirst();
+
+		// Если такое уже есть, удаляем, если нет, создаём
+		if (sharing.isPresent()) {
+			hostSharingRepos.delete(sharing.get());
+			hostSharingRepos.flush();
+		}
+		else {
+			HostSharing hs = new HostSharing();
+			hs.setClient(shared);
+			hs.setHost(hostRepos.findById(id).get());
+			hostSharingRepos.save(hs);
+			hostSharingRepos.flush();
+		}
+		
+		return ResponseEntity.status(HttpStatus.OK).build();
+	}
 }
