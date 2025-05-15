a4929aa084aa2c9924c83defd857fe8122b7bf2c
Tank 228 <92095847+tankoman228@users.noreply.github.com>
2025-05-15 14:11:01 +0700

    Интегрировал аспект и аннотацию для проверки сессий юзеров

---
+++ host/src/main/java/com/cubes_and_mods/host/security/ServiceClientSession.java
@@ -0,0 +1,53 @@
+package com.cubes_and_mods.host.security;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.ExchangeStrategies;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Service
+public class ServiceClientSession {
+
+    @Value("${auth-address}")
+    private String authAddress;
+
+    private final ConcurrentHashMap<String, ClientSession> sessions = new ConcurrentHashMap<>();
+
+    public ClientSession getSession(String token) {
+
+        if (sessions.containsKey(token)) {
+            return sessions.get(token);
+        }
+
+        WebClient webClient = WebClient.builder()
+        .baseUrl(authAddress) 
+        .clientConnector(ClientConnectorForKey.getForKey("auth"))
+        .exchangeStrategies(ExchangeStrategies.builder()
+                .codecs(configurer -> configurer.defaultCodecs().enableLoggingRequestDetails(true))
+                .build())
+        .build();
+        
+        var request = new ProtectedRequest<String>(token);
+
+        try {
+            var r = webClient.post()
+            .uri("/users/get_session")
+            .bodyValue(request)
+            .retrieve()
+            .toEntity(ClientSession.class)
+            .block();
+
+            if (!r.getStatusCode().isError()) {
+                sessions.put(token, r.getBody());
+            }
+
+            return r.getBody();
+        }
+        catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+            return null;
+        }
+    }
+}
