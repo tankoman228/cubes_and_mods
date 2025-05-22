95c645394c9cf73db8e60e7eeb07b52fec84edeb
Tank 228 <92095847+tankoman228@users.noreply.github.com>
2025-05-22 15:12:32 +0700

    Config удалил

--- host/src/main/java/com/cubes_and_mods/host/HostApplication.java
+++ host/src/main/java/com/cubes_and_mods/host/HostApplication.java
@@ -3,15 +3,10 @@ package com.cubes_and_mods.host;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.cubes_and_mods.host.service.Config;
-
 @SpringBootApplication
 public class HostApplication {
 
- public static void main(String[] args) {
-  
-  Config.INIT();
-  
-  SpringApplication.run(HostApplication.class, args);
- }
+    public static void main(String[] args) {
+        SpringApplication.run(HostApplication.class, args);
+    }
 }
