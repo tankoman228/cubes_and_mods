9ac659b1bcb281d4d12fdf7ce376c2b7639cdf16
Tank 228 <92095847+tankoman228@users.noreply.github.com>
2025-05-25 11:59:37 +0700

    Бежать за молоком, доделываю проверку сессий юзеров на servers и host

--- host/src/main/java/com/cubes_and_mods/host/security/SecurityCheckAspect.java
+++ host/src/main/java/com/cubes_and_mods/host/security/SecurityCheckAspect.java
@@ -6,12 +6,15 @@ import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.ApplicationContext;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.*;
 
 import com.cubes_and_mods.host.security.annotations.AllowedOrigins;
+import com.cubes_and_mods.host.security.annotations.CheckUserSession;
+import com.cubes_and_mods.host.security.annotations.CheckUserSession.SessionValidator;
 
 import java.io.Console;
 import java.lang.annotation.Annotation;
@@ -27,6 +30,13 @@ public class SecurityCheckAspect {
 
     @Autowired
     private SecurityCheckingService securityCheckingService;
+
+    @Autowired 
+    private ServiceClientSession sessionClient;
+
+    @Autowired
+    private ApplicationContext ctx;
+
 
     @Value("#{new Boolean('${ignore-source-checking}')}")
     private Boolean IgnoreSourceChecking = false;
@@ -51,6 +61,38 @@ public class SecurityCheckAspect {
             }
         }
         
+        return joinPoint.proceed();
+    }
+
+    @Around("@annotation(check)")
+    public Object around(ProceedingJoinPoint joinPoint, CheckUserSession check) throws Throwable {
+
+        System.out.println("Aspect check client session entered!!!!!!!!!");
+        if (IgnoreSourceChecking) {
+            return joinPoint.proceed();
+        }
+
+        // Ищем ProtectedRequest в аргументах метода
+        for (Object arg : joinPoint.getArgs()) {
+            if (arg instanceof ProtectedRequest) {
+
+                ProtectedRequest<?> protectedRequest = (ProtectedRequest<?>) arg;
+                ClientSession session = sessionClient.getSession(protectedRequest.userSession);
+
+                if (session == null)
+                    return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+                
+
+                SessionValidator validator = (SessionValidator) ctx.getBean(check.validator());
+
+                boolean ok = validator.validate(session, protectedRequest);
+                if (!ok) {
+                    return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+                }
+                break;
+            }
+        }
+
         return joinPoint.proceed();
     }
 }
